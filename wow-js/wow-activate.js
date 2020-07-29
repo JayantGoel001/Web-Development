860f728376860ff53e1f4f05249abdb7914b1d90
Cormac Krum <54479676+CormacKrum@users.noreply.github.com>
2020-07-29 18:48:25 +0530

    Add files via upload

---
+++ wow-js/wow-activate.js
@@ -0,0 +1,8 @@
+var wow = new WOW({
+    boxClass: 'wow', // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset: 0, // distance to the element when triggering the animation (default is 0)
+    mobile: false // trigger animations on mobile devices (true is default)
+  });
+  
+wow.init();
