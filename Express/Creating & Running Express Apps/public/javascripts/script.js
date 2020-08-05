13f37af260fe4d44cdbe6c04e30d4d623cbc5de5
Cormac Krum <54479676+CormacKrum@users.noreply.github.com>
2020-08-05 23:49:27 +0530

    Add files via upload

--- Express/Creating & Running Express Apps/public/javascripts/script.js
+++ Express/Creating & Running Express Apps/public/javascripts/script.js
@@ -1,1 +1,43 @@
 console.log("Express is Working");
+let obj ={
+    "name" :{
+        'firstname' : 'Nancy',
+        'lastname' : 'Benny'
+    },
+    'email' : 'OhhNancy@gmail.com',
+    'age' : 35,
+    'location' : {
+        'country' : 'New Zealand',
+        'city' : 'Auckland',
+        'homeaddress':'40 Pandora Avenue, Sewyn Heights'
+    },
+    'likes':['Swimming','Sleeping']
+};
+
+console.log(obj);
+
+let jsobj ={
+    name :{
+        firstname : 'Nancy',
+        lastname : 'Benny'
+    },
+    email : 'OhhNancy@gmail.com',
+    age : 35,
+    location : {
+        country : 'New Zealand',
+        city : 'Auckland',
+        homeaddress:'40 Pandora Avenue, Sewyn Heights'
+    },
+    likes : ['Swimming','Sleeping']
+};
+console.log(jsobj);
+
+var serializedObj = JSON.stringify(jsobj);
+console.log(serializedObj);
+console.log(typeof serializedObj);
+
+var objAgain = JSON.parse(serializedObj);
+console.log(objAgain);
+console.log(typeof objAgain);
+
+// Serialization : it converts json into string.
