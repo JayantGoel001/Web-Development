8571b125980bfe693ba133bec7a7a7985ea422b3
Cormac Krum <54479676+CormacKrum@users.noreply.github.com>
2020-08-05 23:37:49 +0530

    Add files via upload

--- Express/Creating & Running Express Apps/routes/index.js
+++ Express/Creating & Running Express Apps/routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    // res.send("<h1>Hello World</h1>");
+    res.render('index', { title: 'Index' });
+});
+router.get('/shop', function(req, res, next) {
+    // res.send("<h1>Hello World</h1>");
+    res.render('shop', { title: 'Shop' });
+});
+router.get('/tickets', function(req, res, next) {
+    // res.send("<h1>Hello World</h1>");
+    res.render('tickets', { title: 'Tickets' });
+});
+router.get('/stats', function(req, res, next) {
+    // res.send("<h1>Hello World</h1>");
+    res.render('Stats', { title: 'Stats' });
 });
 
 module.exports = router;
