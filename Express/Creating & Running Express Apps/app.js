b0d470f4f71163f1960c0088b22d2e4bd227a6d2
Cormac Krum <54479676+CormacKrum@users.noreply.github.com>
2020-08-06 22:13:38 +0530

    Add files via upload

--- Express/Creating & Running Express Apps/app.js
+++ Express/Creating & Running Express Apps/app.js
@@ -5,12 +5,13 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const sassMiddleware = require('node-sass-middleware');
 
-const indexRouter = require('./routes/index');
+const indexRouter = require('./mvc/routes/index');
+const postRouter = require('./mvc/routes/posts');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname,'mvc', 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
@@ -26,6 +27,7 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/posts', postRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
