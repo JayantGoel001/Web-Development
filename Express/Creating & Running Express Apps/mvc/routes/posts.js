3502f5937111055665bdda4f8989f754a0b76b45
Cormac Krum <54479676+CormacKrum@users.noreply.github.com>
2020-08-10 22:31:29 +0530

    HTTP

--- Express/Creating & Running Express Apps/mvc/routes/posts.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-const ctrlPosts = require("../controllers/posts");
-
-router.get("/",ctrlPosts.getPosts);
-router.get("/:postid",ctrlPosts.getPost);
-
-module.exports = router;
