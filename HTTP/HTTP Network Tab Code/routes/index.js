c1548c087d2f5478393c5bed5a414edce842ba49
Cormac Krum <54479676+CormacKrum@users.noreply.github.com>
2020-08-10 18:56:00 +0530

    HTTP

---
+++ HTTP/HTTP Network Tab Code/routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    // res.header("Set-Cookie", "secret=express-12345");
+    res.render('index', { title: 'HTTP' });
+});
+
+module.exports = router;
