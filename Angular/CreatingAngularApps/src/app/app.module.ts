5204580f7c491bdbc878b8019fbe9fd63f0989c1
username <54479676+CormacKrum@users.noreply.github.com>
2020-08-13 22:28:26 +0530

    Creating Components

--- Angular/CreatingAngularApps/src/app/app.module.ts
+++ Angular/CreatingAngularApps/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app/app.component';
+import { AiComponent } from './ai/ai.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        AiComponent,
+        FormComponent
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
